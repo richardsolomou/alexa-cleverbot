af6f5c277cf8b2c1f8489c3a28ecfd1156144f3e
Richard Solomou <Richard.Solomou@efficioconsulting.com>
2017-02-12 17:22:51 +0000

    Fixed cleverbot responses, added object[]

--- alexa/bot.js
+++ alexa/bot.js
@@ -1,24 +1,20 @@
 const cleverbot = require('cleverbot.io');
 let bot;
 
-if (!process.env.cleverbot) {
-  console.log('#\r\n# Failed to find cleverbot object in `env.json`\r\n#');
-}
-
-if (!process.env.cleverbot.user) {
+if (!process.env.cleverbot_user) {
   console.log('#\r\n# Failed to find cleverbot user in `env.json`\r\n#');
 }
 
-if (!process.env.cleverbot.key) {
+if (!process.env.cleverbot_key) {
   console.log('#\r\n# Failed to find cleverbot key in `env.json`\r\n#');
 }
 
-if (!process.env.cleverbot.nick) {
+if (!process.env.cleverbot_nick) {
   console.log('#\r\n# Failed to find cleverbot nick in `env.json`\r\n#');
 }
 
-bot = new cleverbot(process.env.cleverbot.user, process.env.cleverbot.key);
-bot.setNick(process.env.cleverbot.nick);
+bot = new cleverbot(process.env.cleverbot_user, process.env.cleverbot_key);
+bot.setNick(process.env.cleverbot_nick);
 
 bot.create(function (err, session)
 {
