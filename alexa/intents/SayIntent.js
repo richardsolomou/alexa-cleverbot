e9d90d4e13ca0a7da82731f79f566be633c1b5ee
Richard Solomou <Richard.Solomou@efficioconsulting.com>
2017-02-12 17:17:25 +0000

    Added session model and interaction saving

--- alexa/intents/SayIntent.js
+++ alexa/intents/SayIntent.js
@@ -1,17 +1,21 @@
-const bot = require('../bot');
+const bot     = require('../bot');
+const session = require('../session');
 
 module.exports = function (slots, callback)
 {
   let slot = 'Hello';
+  let promise;
 
   if (slots.say && slots.say.value) {
     slot = slots.say.value;
   }
 
-  const promise = bot(slot);
+  promise = bot(slot);
 
   promise.then(function (val)
   {
+    session.addInteraction(slot, val);
+
     return callback(null, val);
   });
 };
---
+++ alexa/session.js
@@ -0,0 +1,26 @@
+const session = {
+  conversations: []
+};
+
+module.exports = {
+  /**
+   * Add interaction
+   *
+   * @param {String} request
+   * @param {String} response
+   */
+  addInteraction: function (request, response)
+  {
+    session.conversations.push(request, response);
+  },
+
+  /**
+   * Get session
+   *
+   * @returns {Object}
+   */
+  getSession: function ()
+  {
+    return session;
+  }
+};
